7a255fb2ff5d22ea8ba20eace5fe9d7a95bcbc4e
onkar-ghule <onakrghule032@gmail.com>
2025-08-30 15:36:48 +0530

    understanding variable rule

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 ##Flask app routing 
 ##create a simple flask application
 app=Flask(__name__)
@@ -9,5 +9,14 @@ def welcome():
 @app.route('/index')
 def index():
   return "welcome to the web page index page"
+@app.route('/success/<int:score>')
+#variable rule 
+#1.the data is by default string we can change its type  
+def success(score):
+  return "the person has passe with score:"+str(score)
+@app.route('/fail/<score>')
+def fail(score):
+  return "the person has fail with score:"+score
+
 if __name__=="__main__":
-  app.run(debug=True)
+  app.run(debug=True) 

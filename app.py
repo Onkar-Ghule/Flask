61411b31daa18810dad918e2b462abcd88725291
onkar-ghule <onakrghule032@gmail.com>
2025-08-30 15:47:27 +0530

    understanding the render templets along with get and post requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 ##Flask app routing 
 ##create a simple flask application
 app=Flask(__name__)
@@ -17,6 +17,15 @@ def success(score):
 @app.route('/fail/<score>')
 def fail(score):
   return "the person has fail with score:"+score
-
+@app.route("/form",methods=['GET','POST'])
+def form():
+  if request.method=="GET":
+    return render_template("form.html")
+  else:
+    maths=float(request.form["maths"])
+    science=float(request.form["science"])
+    history=float(request.form["history"])
+    avg_marks=(maths+science+history)/3
+    return render_template('form.html',score=avg_marks)
 if __name__=="__main__":
   app.run(debug=True) 
